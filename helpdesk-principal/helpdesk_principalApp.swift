22206384f76e97ac4c360330d097e6146fb38165
Arlid Henao Rueda <arlidhenaorueda@gmail.com>
2024-01-20 14:34:15 -0500

    Initial Commit

---
+++ helpdesk-principal/helpdesk_principalApp.swift
@@ -0,0 +1,17 @@
+//
+//  helpdesk_principalApp.swift
+//  helpdesk-principal
+//
+//  Created by Arlid Henao Rueda on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct helpdesk_principalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
